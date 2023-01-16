cceaac70b435a307b7d5f9a85a99527d63702803
cmkds <107685502+cmkds@users.noreply.github.com>
2023-01-16 21:10:36 +0900

    230116

--- 일기장프로젝트/src/App.js
+++ 일기장프로젝트/src/App.js
@@ -1,46 +1,44 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor2 from "./DiaryEditor2";
 import DiaryList from "./DiaryList";
-import Lifecycle from "./Lifecycle";
 
-// const dummyList = [
-//   {
-//     id: 1,
-//     author: "김동수",
-//     content: "하이1",
-//     emotion: 5,
-//     created_date: new Date().getTime(), // new는 생성자를 생성 할 때 쓰인다. new로 시간 객체를 생성한 것.
-//     //Date.getTime()은 시간을 숫자로 반환해주는 함수.
-//   },
-//   {
-//     id: 2,
-//     author: "서동수",
-//     content: "하이2",
-//     emotion: 3,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 3,
-//     author: "박동수",
-//     content: "하이3",
-//     emotion: 4,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 4,
-//     author: "이동수",
-//     content: "하이4",
-//     emotion: 2,
-//     created_date: new Date().getTime(),
-//   },
-// ];
 
 function App() {
   const [data, setData] = useState([]);
   //일기 데이터를 넣을 것이기 때문에 useState는 배열로 초기값 설정.
 
   const dataId = useRef(0);
+  //@@@ API 호출하기
+  //async 코드를 붙혀서 getData객체를 포로미스를 반환하는 비동기 함수로 만든다.
+  //await fetch('api 주소').then((res)=> res.json())
+  const getData = async() => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/comments').then((res)=> res.json())
+    //res를 0~19까지 자름
+    //여기선 api를 우리가 만든 일기 툴에 넣기 위해 map을 이용
+    const initData = res.slice(0,20).map((it)=>{
+      return {
+        
+        author :it.email,
+        content : it.body,
+        emotion : Math.floor(Math.random() * 5)+1,
+        //자바스크립트의 내장 객체 사용.
+        // Math.random()은 0~1까지 랜덤 난수 생성
+        // Math.floor() 소수점을 전부 버려주는 함수
+        created_date : new Date().getTime(),
+        id : dataId.current++ // dataId를 현재 current값으로 넣고 나서 1증가
+      }
+    })
+
+    setData(initData)
+  }
+
+  // API 를 호출하는 getData 함수를 마운트 되자마자 호출하기
+  useEffect(()=> {
+    getData()
+  }, [])
+
+
 
   const onCreate = (author, content, emotion) => {
     //객체에 담을 값들은 파라미터로 받아 온다.
@@ -79,14 +77,27 @@ function App() {
     );
   };
 
+
+  const getDiaryAnalysis = () =>{
+    console.log("일기 분석 시작")
+
+    const goodCount = data.filter((it) => it.emotion >=3).length
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100
+    return {goodCount, badCount, goodRatio}
+  }
+
+  const {goodCount, badCount, goodRatio} = getDiaryAnalysis();
+
   return (
     <div className="App">
-      <Lifecycle />
 
       <DiaryEditor2 onCreate={onCreate} />
-      {/* onCreate 함수를 diaryEditor의 프롭으로 내려줌 */}
+      <div>전체일기 : {data.length}</div>
+      <div>기분 좋은 일기 개수 : {goodCount}</div>
+      <div>기분 나쁜 일기 개수 : {badCount}</div>
+      <div>기분 좋은 일기 비율 : {goodRatio}</div>
       <DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit} />
-      {/* DiaryItem에서 ondelete를 실행시키기 위해서 DiaryList에 프롭스로 보내준다. */}
     </div>
   );
 }
--- 일기장프로젝트/src/Lifecycle.js
+++ 일기장프로젝트/src/Lifecycle.js
@@ -1,63 +1,30 @@
 import React, { useEffect, useState } from "react";
 
-const Lifecycle = () => {
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState("");
-
-  //리액트의 기능 useEffect 사용
-
-  // Mount
-  // Mount가 되는 시점에 무언가를 하고 싶다면
-  // 2번째  인자에 빈배열을 넣어주고
-  // 1번째 인자 콜백함수에 하고 싶은 작업을 하면 된다.
-  useEffect(() => {
+const UnmountTest = () => {
+  //컴포넌트가 언마운트 되는 순간 제어 하기
+  //기본적으로 useEffect(() => {},[])를 사용하면 업데이트 될 때 동작한다.
+  useEffect(()=> {
     console.log("Mount!");
-  }, []);
 
-  // Update
-  // 모든 Update
-  // 2번째 인자에 아무것도 작성하지 않는다.
-  useEffect(() => {
-    console.log("Update!");
-  });
-
-  // Update
-  // 특정 State 업데이트. count
-  useEffect(() => {
-    console.log(`count is update : ${count}`);
-    if (count > 5) {
-      alert("count가 5를 넘었습니다 따라서 1로 초기화합니다.");
-      setCount(1);
-      //setCount(실행)
+    //리턴값에 원하는 것을 콜백함수로 넣으면 
+    //Unmount 시점에 실행 시킬 수 있다.
+    return () =>{
+      // Unmount 시점에 실행되게 됨
+      console.log("Unmount!"); 
     }
-  }, [count]);
+  },[])
+}
 
-  // Update
-  // 특정 State 업데이트. text
-  useEffect(() => {
-    console.log(`text is update ${text}`);
-  }, [text]);
+const Lifecycle = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const toggle = () => setIsVisible(!isVisible);
 
   return (
     <div style={{ padding: 20 }}>
-      <div>
-        {count}
-        <button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          +
-        </button>
-      </div>
-      <div>
-        <input
-          value={text}
-          onChange={(e) => {
-            setText(e.target.value);
-          }}
-        />
-      </div>
+      <button onClick={toggle}>ON/OFF</button>
+      {isVisible && <UnmountTest/>}
+      {/* 단락회로 평가 isVisible이 false면 뒤에껄 확인안하기에 UnmontTest가
+      안나옴. True면 뒤의걸 확인하기 때문에 UnmountTest 컴포넌트가 나타남 */}
     </div>
   );
 };
